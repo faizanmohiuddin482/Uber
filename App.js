2ff2889ac031cff9e55df77046d55a885e71dc00
Mohammed Faizan Mohiuddin <faizanmohiuddin482@gmail.com>
2021-08-07 02:31:41 +0530

    added redux, tailwind css

--- App.js
+++ App.js
@@ -1,13 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {Provider} from 'react-redux';
+import {store} from './store';
+import HomeScreen from './screens/HomeScreen';
+
+// 1) Setup redux
 
 export default function App() {
   return (
+    <Provider store={store}>
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+     <HomeScreen/>
     </View>
+    </Provider>
   );
 }
 
@@ -15,7 +21,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
